4cee648ebfc422b42912718f8cc1bf6215447993
Tim <tbarry2@andrew.cmu.edu>
2022-07-11 13:42:44 -0400

    add queue info

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,15 @@
-# Note: This script should be run on an HPC with at least 250GB of RAM.
-# Note: This script requires the R packages readr, dplyr, ondisc, readxl, Seurat, lowmoi
+# required R packages readr, dplyr, ondisc, readxl, Seurat, lowmoi
 
 #$ -l m_mem_free=250G
-module load R/R-4.1.2
-source ~/.research_config
+#$ -q short.q
+
+hpcc=$(hostname | grep "hpcc" | wc -l)
+if [[ hpcc ]]
+then
+  module load R/R-4.1.2
+fi
+
+# source ~/.research_config
 # source 1_download_data.sh
 Rscript 2_convert_to_odm.R
 Rscript 3_calculate_cell_cycle.R
